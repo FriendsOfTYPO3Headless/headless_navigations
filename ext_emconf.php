be899dde169690ae64a0f301fb62ac6143dd5292
svenpetersen <sp@dauskonzept.de>
2022-02-07 12:10:16 +0100

    Updated EXT requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,11 +13,11 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.5.99',
+            "headless" => "2.0.0-3.0.99",
         ],
         'conflicts' => [
         ],
         'suggests' => [
-            'headless' => '2.6.0-3.0.99'
         ],
     ],
 ];
